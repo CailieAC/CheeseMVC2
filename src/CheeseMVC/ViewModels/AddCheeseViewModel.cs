fba5d988dfc3a992340fde2b89074a8616fa4788
CailieAC <cailiecarlile@gmail.com>
2019-06-02 19:59:32 -0500

    ViewModels and enums

---
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must give your cheese a description")]
+        public string Description { get; set; }
+
+        //represents the user's selected cheese type
+        public CheeseType Type { get; set; }
+
+        //Choices the user has for cheese types
+        public List<SelectListItem> CheeseTypes { get; set; }
+
+        //Add a constructor
+        public AddCheeseViewModel()
+        {
+            CheeseTypes = new List<SelectListItem>();
+
+            //Would ideally make a loop for this
+            //<option value ="0">Hard</option>
+            CheeseTypes.Add(new SelectListItem
+            {
+                Value = ((int)CheeseType.Hard).ToString(),
+                Text = CheeseType.Hard.ToString()
+            });
+
+            CheeseTypes.Add(new SelectListItem
+            {
+                Value = ((int)CheeseType.Soft).ToString(),
+                Text = CheeseType.Soft.ToString()
+            });
+
+            CheeseTypes.Add(new SelectListItem
+            {
+                Value = ((int)CheeseType.Fake).ToString(),
+                Text = CheeseType.Fake.ToString()
+            });
+        }
+    }
+}
