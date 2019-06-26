d1632c6c90fc4d411b8f237260347ab727933ce6
Cailie Carlile <cailiecarlile@gmail.com>
2019-06-26 18:09:30 -0500

    Add CheeseCategory and AddCategoryViewModel

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CheeseMVC.Controllers
@@ -20,6 +22,33 @@ namespace CheeseMVC.Controllers
         {
             context.Categories.ToList();
             return View(context);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddCategoryViewModel viewModel = new AddCategoryViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                CheeseCategory newCategory = new CheeseCategory()
+                {
+                    Name = viewModel.Name,
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(viewModel);
         }
     }
 }
